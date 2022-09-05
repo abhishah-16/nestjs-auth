679a9cc536cb552c429575b9b5a44eb195fa5ce3
abhishah <abhishah0961@gmail.com>
2022-09-05 16:09:15 +0530

    added: dto & signin method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authservice: AuthService) {
+
+    }
+
+    @Post('/local/signup')
+    signup(@Body() dto:AuthDto) {
+        this.authservice.signup(dto)
+    }
+
+    @Post('/local/signin')
+    signin() {
+        this.authservice.signin()
+    }
+
+    @Post('/logout')
+    logout() {
+        this.authservice.logout()
+    }
+
+    @Post('/refresh')
+    refreshToken() {
+        this.authservice.refreshToken()
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private prisma: PrismaService) { }
+
+    signup(dto: AuthDto) {
+        // const newuser = this.prisma.user.create({
+        //     data:{
+        //         email: dto.email,
+
+        //     }
+        // })
+    }
+
+    signin() {
+
+    }
+
+    logout() {
+
+    }
+
+    refreshToken() {
+
+    }
+}
