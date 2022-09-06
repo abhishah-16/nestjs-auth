ed6ceed4080441defa6a49c8608318fe51246234
abhishah <abhishah0961@gmail.com>
2022-09-06 15:13:23 +0530

    added: signup method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,25 +3,69 @@ import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt'
 import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private prisma: PrismaService) { }
+    constructor(private prisma: PrismaService,
+        private jwtservice: JwtService) { }
 
-    hashData(data: string) {
+    // hash data
+    hashData(data: any) {
         return bcrypt.hash(data, 10)
     }
 
-    async signup(dto: AuthDto):Promise<Tokens> {
-        const hash = await this.hashData(dto.password)
-        const newuser = this.prisma.user.create({
+    // generate and get token
+    async getToken(userId: number, email: string) {
+        const [at, rt] = await Promise.all([
+            this.jwtservice.signAsync({
+                sub: userId,
+                email
+            }, {
+                secret: 'aFvbniiNIUBBF2349284FJNV',
+                expiresIn: 60 * 15
+            }),
+            this.jwtservice.signAsync({
+                sub: userId,
+                email
+            }, {
+                secret: 'aFvbniiNIUBBF2349284FJNV',
+                expiresIn: 60 * 60 * 24 * 7
+            })
+        ])
+
+        return {
+            access_token: at,
+            refresh_token: rt
+        }
+
+    }
+
+    // hash refresh token and update user data
+    async updateRtHash(userId: number, refreshToken: string) {
+        const hash = await this.hashData(refreshToken)
+        await this.prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                hashedRt: hash
+            }
+        })
+    }
+
+    async signup(dto: AuthDto): Promise<Tokens> {
+        const hash = await this.hashData(dto.password.toString())
+        const newuser = await this.prisma.user.create({
             data: {
                 email: dto.email,
                 hash
             }
         })
-        return null
+        const tokens = await this.getToken(newuser.id, newuser.email)
+        await this.updateRtHash(newuser.id, tokens.refresh_token)
+        return tokens
     }
 
     signin() {
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -5,7 +5,6 @@ export class AuthDto {
     @IsNotEmpty()
     email: string;
 
-    @IsEmail()
     @IsNotEmpty()
     password: string
 }
